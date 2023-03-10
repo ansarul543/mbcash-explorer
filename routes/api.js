32bfd08fcfa417bd8f31690b2ea717492ab32916
Ansarul Mullah <mdansarul543@gmail.com>
2023-03-10 11:08:15 +0600

    okkk

--- routes/api.js
+++ routes/api.js
@@ -175,6 +175,7 @@ router.get("/trxs", (req, res) => {
       sorts = 1
     }
   }
+
   if (req.query.page !== "" && req.query.page !== undefined) {
     page = (parseInt(req.query.page)* limits) -1
   }
@@ -186,7 +187,12 @@ router.get("/trxs", (req, res) => {
       result.forEach(function (item, index) {
         trx.push(item.data)
       })
-      res.json(trx)
+      let data = new Map();
+      for (let obj of trx) {
+        data.set(obj.hash, obj);
+      }
+      let out = [...data.values()];
+      res.json(out)
     }
   })
 
